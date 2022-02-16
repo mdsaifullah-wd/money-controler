b92b1e5e3792c3813aa486babcbaf20ab81d0206
mdsaifullah.wd <mdsaifullah.wd@gmail.com>
2022-02-17 04:35:16 +0600

    add error handling feature

--- js/app.js
+++ js/app.js
@@ -23,13 +23,41 @@ document
 
     // Calculate Total Expenses
     const totalExpenses = foodExpenses + rentExpenses + clothesExpenses;
-    // Set Total Expenses as the innerText of Total Expenses Field
-    document.getElementById("total-expenses").innerText = totalExpenses;
 
-    // Calculate the balance
-    const balance = income - totalExpenses;
-    // Set the balance as the innerText of Balance Field
-    document.getElementById("balance").innerText = balance;
+    // Error Handling when the values are not numbers
+    if (
+      isNaN(income) ||
+      isNaN(foodExpenses) ||
+      isNaN(rentExpenses) ||
+      isNaN(clothesExpenses)
+    ) {
+      document.getElementById("error-field-one").innerText =
+        "Please, Enter a valid number!";
+    }
+    // Error handling when the values are negative
+    else if (
+      income < 0 ||
+      foodExpenses < 0 ||
+      rentExpenses < 0 ||
+      clothesExpenses < 0
+    ) {
+      document.getElementById("error-field-one").innerText =
+        "Please, Enter a positive value!";
+    }
+    // Error handling when expenses is greater than income
+    else if (totalExpenses > income) {
+      document.getElementById("error-field-one").innerText =
+        "You cannot expense more than " + income + "tk!";
+    }
+    // Generate the result when no errors found
+    else {
+      // Set Total Expenses as the innerText of Total Expenses Field
+      document.getElementById("total-expenses").innerText = totalExpenses;
+      // Calculate the balance
+      const balance = income - totalExpenses;
+      // Set the balance as the innerText of Balance Field
+      document.getElementById("balance").innerText = balance;
+    }
   });
 
 // Add event listener to the save button
@@ -45,11 +73,26 @@ document.getElementById("save-button").addEventListener("click", function () {
 
   // Calculate Saving Amount
   const savingAmount = income * (savingRate / 100);
-  // Set the Saving Amount as the innerText of Saving Ammount Field
-  document.getElementById("saving-amount").innerText = savingAmount;
 
-  // Calculate Remaining Balance
-  const remainingBalance = balance - savingAmount;
-  // Set the remaining balance as the innerText of remaining balance field
-  document.getElementById("remaining-balance").innerText = remainingBalance;
+  // Error Handling when saving rate is not a valid number
+  if (isNaN(savingRate) || savingRate < 0) {
+    document.getElementById("error-field-two").innerText =
+      "Please, Enter a valid saving rate!";
+  }
+  // Error handling when saving amount is greater than balance
+  else if (savingAmount > balance) {
+    document.getElementById("error-field-two").innerText =
+      "You don't have sufficient balance for saving " + savingRate + "%";
+    // So remaining balance will be equals to balance
+    document.getElementById("remaining-balance").innerText = balance;
+  }
+  // Genarate the result when no errors found
+  else {
+    // Set the Saving Amount as the innerText of Saving Ammount Field
+    document.getElementById("saving-amount").innerText = savingAmount;
+    // Calculate Remaining Balance
+    const remainingBalance = balance - savingAmount;
+    // Set the remaining balance as the innerText of remaining balance field
+    document.getElementById("remaining-balance").innerText = remainingBalance;
+  }
 });
